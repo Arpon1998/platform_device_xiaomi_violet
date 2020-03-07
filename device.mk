2ae8e315114e3b5e3c3ab45cd74ca0dfc21afeed
Arpon Sarkar <officialkevin8768@gmail.com>
2020-03-07 05:32:41 +0000

    violet: Nuked ENFORCE_RRO_TARGETS

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-pe
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
+#PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     NotchBarKiller
